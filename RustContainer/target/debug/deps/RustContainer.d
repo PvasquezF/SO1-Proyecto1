2d0038c0b1b605225b05c598e44a5edc7999b456
PvasquezF <alexanderpavelv32@gmail.com>
2020-04-11 03:41:35 -0600

    Inicializando rust

---
+++ RustContainer/target/debug/deps/RustContainer.d
@@ -0,0 +1,5 @@
+C:\Users\Pavel\source\repos\SO1-Proyecto1\RustContainer\target\debug\deps\RustContainer.exe: src\main.rs
+
+C:\Users\Pavel\source\repos\SO1-Proyecto1\RustContainer\target\debug\deps\RustContainer.d: src\main.rs
+
+src\main.rs:
