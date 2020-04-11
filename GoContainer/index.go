78e6a813c4c804477be675eba80875f236f13a78
PvasquezF <alexanderpavelv32@gmail.com>
2020-04-10 20:41:14 -0600

    Grafica Ram
    Grafica de ram en go terminada

--- GoContainer/index.go
+++ GoContainer/index.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // Data struct
@@ -17,64 +24,66 @@ type Data struct {
 
 // RAM struct
 type RAM struct {
-	Total string  `json:"total"`
-	Usage float64 `json:"usage"`
+	Total string `json:"total"`
+	Usage int64  `json:"usage"`
 }
 
 // CPU struct
 type CPU struct {
-	Cores  int32   `json:"cores"`
-	Vendor string  `json:"vendor"`
-	Family string  `json:"family"`
-	Model  string  `json:"model"`
-	Speed  string  `json:"speed"`
-	Read   float64 `json:"read"`
+	Cores  int32  `json:"cores"`
+	Vendor string `json:"vendor"`
+	Family string `json:"family"`
+	Model  string `json:"model"`
+	Speed  string `json:"speed"`
+	Read   string `json:"read"`
 }
 
-type Todo struct {
-	Title string
-	Done  bool
+// TodoPage struct
+type TodoPage struct {
+	Times   [100]string
+	Values  [100]string
+	Valores string
+	Tiempos string
+	Index   int
 }
 
-type TodoPageData struct {
-	PageTitle string
-	Todos     []Todo
+var data TodoPage
+var index = 0
+
+func showPage(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("layout.html")
+	responseObject := getData()
+
+	data.Times[index] = time.Now().Format("01-02-2006 15:04:05")
+	data.Values[index] = strconv.FormatInt(responseObject.Ram.Usage, 10)
+	index = index + 1
+	data.Index = index
+	data.Valores = strings.Join(data.Values[:], ",")
+	data.Tiempos = strings.Join(data.Times[:], ",")
+	tmpl.Execute(w, data)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func getData() Data {
+	response, err := http.Get("http://localhost:8080/")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseObject Data
+	json.Unmarshal(responseData, &responseObject)
+	return responseObject
 }
 
 func main() {
-	//response, err := http.Get("http://localhost:8080/")
-	//
-	//if err != nil {
-	//	fmt.Print(err.Error())
-	//	os.Exit(1)
-	//}
-	//
-	//responseData, err := ioutil.ReadAll(response.Body)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//var responseObject Data
-	//json.Unmarshal(responseData, &responseObject)
-	//fmt.Println(responseObject.Os)
-	//http.HandleFunc("/", handler)
-	//log.Fatal(http.ListenAndServe(":8081", nil))
-
-	tmpl := template.Must(template.ParseFiles("layout.html"))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := TodoPageData{
-			PageTitle: "My TODO list",
-			Todos: []Todo{
-				{Title: "Task 1", Done: false},
-				{Title: "Task 2", Done: true},
-				{Title: "Task 3", Done: true},
-			},
-		}
-		tmpl.Execute(w, data)
-	})
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	time.Sleep(2 * time.Second)
+	http.HandleFunc("/", showPage)
 	http.ListenAndServe(":8081", nil)
 }
