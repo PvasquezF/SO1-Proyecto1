05d74666ae20644cbc4c33668728332290a374c3
PvasquezF <alexanderpavelv32@gmail.com>
2020-04-11 00:28:34 -0600

    Redis conectado
    Se realizo la conexion con redis

--- GoContainer/index.go
+++ GoContainer/index.go
@@ -11,6 +11,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/go-redis/redis"
 )
 
 // Data struct
@@ -47,20 +49,66 @@ type TodoPage struct {
 	Index   int
 }
 
-var data TodoPage
-var index = 0
+// Redis data struct
+type RedisData struct {
+	Valor  string
+	Tiempo string
+}
+
+type redisClient struct {
+	c *redis.Client
+}
+
+var (
+	client = &redisClient{}
+	data   TodoPage
+	index  = 0
+)
 
 func showPage(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles("layout.html")
 	responseObject := getData()
-
-	data.Times[index] = time.Now().Format("01-02-2006 15:04:05")
-	data.Values[index] = strconv.FormatInt(responseObject.Ram.Usage, 10)
+	v1 := time.Now().Format("01-02-2006 15:04:05")
+	v2 := strconv.FormatInt(responseObject.Ram.Usage, 10)
+	data.Times[index] = v1
+	data.Values[index] = v2
 	index = index + 1
 	data.Index = index
 	data.Valores = strings.Join(data.Values[:], ",")
 	data.Tiempos = strings.Join(data.Times[:], ",")
+
+	redisClient := initialize()
+	key1 := "ram"
+	value1 := RedisData{Valor: v2, Tiempo: v1}
+	err := redisClient.setKey(key1, value1, time.Minute*1)
+	if err != nil {
+		log.Fatalf("Error: %v", err.Error())
+	}
 	tmpl.Execute(w, data)
+}
+
+func initialize() *redisClient {
+	c := redis.NewClient(&redis.Options{
+		Addr: "35.208.144.20:6379",
+	})
+
+	if err := c.Ping().Err(); err != nil {
+		panic("Unable to connect to redis " + err.Error())
+	}
+	client.c = c
+	return client
+}
+
+func (client *redisClient) setKey(key string, value interface{}, expiration time.Duration) error {
+	cacheEntry, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	err = client.c.LPush(key, cacheEntry).Err()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func getData() Data {
