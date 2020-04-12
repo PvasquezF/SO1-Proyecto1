f8f5b65ac4713756692808afeca4631ed7a7599d
PvasquezF <alexanderpavelv32@gmail.com>
2020-04-11 18:15:39 -0600

    add reading file

--- RustContainer/src/main.rs
+++ RustContainer/src/main.rs
@@ -6,6 +6,8 @@ extern crate rouille;
 use maud::html;
 use rouille::Response;
 use rouille::router;
+use std::env;
+use std::fs;
 
 fn main() {
     rouille::start_server("0.0.0.0:8888", move |request| {
@@ -15,6 +17,10 @@ fn main() {
                 let markup = html! {
                     p { "Hi, " (name) "!" }
                 };
+                let contents = fs::read_to_string("templates/base.html")
+                .expect("Something went wrong reading the file");
+
+                println!("With text:\n{}", contents);
                 return Response::html(markup.into_string());
             },
             _ => Response::empty_404()
