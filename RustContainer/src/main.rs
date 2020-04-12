73e497cc43b1c78fc5ac7e27a8b66a02b3d18ed7
PvasquezF <alexanderpavelv32@gmail.com>
2020-04-11 19:47:12 -0600

    request rust

--- RustContainer/src/main.rs
+++ RustContainer/src/main.rs
@@ -2,12 +2,14 @@
 #![feature(proc_macro_hygiene)]
 extern crate maud;
 extern crate rouille;
+extern crate redis;
 
 use maud::html;
 use rouille::Response;
 use rouille::router;
 use std::env;
 use std::fs;
+use reqwest::r#async::{Client, Decoder};
 
 fn main() {
     rouille::start_server("0.0.0.0:8888", move |request| {
@@ -16,14 +18,25 @@ fn main() {
                 let name = "Lyra";
                 let contents = fs::read_to_string("Template/index.html")
                 .expect("Something went wrong reading the file");
-                let markup = html! {
-                    (contents)
-                };
+                let body = reqwest::get("http://35.208.41.153:8080")
+                .await?
+                .text()
+                .await?;
 
-                println!("With text:\n{}", contents);
+                println!("body = {:?}", body);
                 return Response::html(contents.into_string());
             },
             _ => Response::empty_404()
         )
     });
+}
+
+fn do_something() -> redis::RedisResult<()> {
+    let client = redis::Client::open("redis://http://35.208.41.153:6379")?;
+    let mut con = client.get_connection()?;
+
+    let _ : () = con.lpush("cpu",val)?;
+    /* do something here */
+
+    Ok(())
 }
