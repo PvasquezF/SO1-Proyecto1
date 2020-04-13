4ff0195f0a38b75aea2ec7680d001a6b8891dd0e
PvasquezF <alexanderpavelv32@gmail.com>
2020-04-12 18:05:23 -0600

    app de python terminada

--- PythonContainer/index.py
+++ PythonContainer/index.py
@@ -4,19 +4,26 @@ import json
 import redis
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/data')
 def hello_world():
     r = redis.Redis(host='35.208.41.153', port=6379, db=0)
     p = r.lrange('cpu', 0, -1)
+    q = r.lrange('ram', 0, -1)
     cpuarray = []
-
+    ramarray = []
     for i in p:
         tupla = str(i).split('|')
-        print(tupla)
         valor = tupla[0].replace('b\'','')
         tiempo = tupla[1].replace('\'','')
-        dataCpu = {"valor": valor,
-                    "tiempo": tiempo}
-        cpuarray.append(dataCpu)
-    print(cpuarray)
+        dataCpu = '{"valor": "'+valor+'", "tiempo": "'+tiempo+'"}'
+        cpuarray.append(json.loads(dataCpu))
+
+    for i in q:
+        tupla = str(i).replace('b\'','')
+        tupla = tupla.replace('\'', '')
+        ramarray.append(json.loads(tupla))
+    return {"ram": ramarray, "cpu": cpuarray}
+
+@app.route('/')
+def index():
     return render_template("index.html")
